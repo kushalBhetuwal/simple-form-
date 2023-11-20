50ae337d0b3248417fc19ab9cce0e6b04524b60d
kushal <kushalbhetuwal@gmail.com>
2023-11-20 21:29:21 +1100

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const App =()=>{
+const[score, setScore] = useState(0);
+const[message, setMessage] = useState("");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  if(Number(score)<5 && message.length<10){
+    alert("please provide us feedback")
+   return;
+  }
+  alert("form has been submitted")
+}
+  return(
+    <form onSubmit ={handleSubmit}>
+    <div>
+    <h1>Give us Feedback</h1>
+    <p>Score:{score}</p>
+    <input type='range' min="0" max="10" onChange={(e)=>setScore(e.target.value)}/>
+      <textarea rows="8" cols="100" onChange={(e)=>setMessage(e.target.value)}></textarea><br/>
+      <button type="submit">Submit</button>
     </div>
-  );
+    </form>
+     
+  )
 }
 
 export default App;
